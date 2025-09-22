c0d31a31bb53659440f5a34280e38d1f0dd3ad42
Andres <andres@tudominio.com>
2025-09-22 04:04:02 +0000

    feat: implementación de Google Tag Manager y Google Analytics usando next/script

--- src/app/(innerpage)/layout.jsx
+++ src/app/(innerpage)/layout.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Header4 from '../Components/Header/Header4';
 import Footer from '../Components/Footer/Footer';
+import GoogleAnalytics from '../Components/Analytics/GoogleAnalytics';
+import GoogleTagManager from '../Components/Analytics/GoogleTagManager';
 
 const DefalultLayout = ({ children }) => {
     return (
         <div className='main-page-area'>
-           <Header4></Header4>
+            <Header4></Header4>
+            <GoogleAnalytics GA_MEASUREMENT_ID="G-XXXXXXXXXX" />
+            <GoogleTagManager GTM_ID="GTM-XXXXXXX" />
+            <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
+                />
+            </noscript>
             {children}
             <Footer></Footer>
         </div>
