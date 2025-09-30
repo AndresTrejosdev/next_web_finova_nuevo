4712811405e9e742b9af05749282f3b01cc3eb93
Andres <andres@tudominio.com>
2025-09-30 19:38:51 +0000

    meta actualización

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import "slick-carousel/slick/slick.css";
 import "./assets/main.css";
 import { GoogleTagManager } from './components/GoogleTagManager';
+import Script from 'next/script';
 
 const urbanist = Urbanist({
   subsets: ['latin'],
@@ -120,6 +121,26 @@ export default function RootLayout({ children }) {
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
    
         <GoogleTagManager />
+
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '767787196141415');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className={`${urbanist.variable} ${nunito.variable}`}>
       
@@ -132,6 +153,16 @@ export default function RootLayout({ children }) {
           />
         </noscript>
         {children}
+        {/* Meta Pixel noscript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=767787196141415&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
       </body>
     </html>
   );
